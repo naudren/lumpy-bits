24195f282805bbbac2172d7412d745bb8348dc11
naudren <nicolas.audren@gmail.com>
2016-05-15 23:08:49 +0100

    Added error code

---
+++ DataStructsAndAlgos/DataStructsAndAlgos/ErrorCodes.h
@@ -0,0 +1,16 @@
+//
+//  ErrorCodes.h
+//  DataStructsAndAlgos
+//
+//  Created by Nicolas Audren on 15/05/2016.
+//  Copyright © 2016 Nicosoft. All rights reserved.
+//
+
+#ifndef ErrorCodes_h
+#define ErrorCodes_h
+
+enum {
+    LINKED_LIST_TEST_FAILED = 1,
+};
+
+#endif /* ErrorCodes_h */
